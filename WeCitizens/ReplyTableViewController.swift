764496d20e0b5dbdcd1bcd65900c35c90136f8af
Harold LIU <7lxd@tongji.edu.cn>
2016-03-04 00:01:41 +0800

    UPDATE RESPONSE Version 2.0

--- WeCitizens/ReplyTableViewController.swift
+++ WeCitizens/ReplyTableViewController.swift
@@ -12,7 +12,7 @@ import FoldingCell
 class ReplyTableViewController: UITableViewController,SSRadioButtonControllerDelegate {
 
     let kRowsCount = 10
-    let kCloseCellHeight:CGFloat = 168
+    let kCloseCellHeight:CGFloat = 280
     let kOpenCellHeight:CGFloat = 850
     
     var cellHeights = [CGFloat]()
@@ -22,15 +22,39 @@ class ReplyTableViewController: UITableViewController,SSRadioButtonControllerDel
         super.viewDidLoad()
         
         
-        
         for _ in 0...kRowsCount {
             cellHeights.append(kCloseCellHeight)
         }
     }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.backgroundColor = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0)
     }
+    
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return kRowsCount
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat
+    {
+        return 7
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
+    {
+        let view = UIView(frame: (CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 7)) )
+        view.backgroundColor = UIColor.clearColor()
+        return view
+    }
+    
+    
+    
     
     override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         
@@ -50,16 +74,8 @@ class ReplyTableViewController: UITableViewController,SSRadioButtonControllerDel
     {
         let cell = tableView.dequeueReusableCellWithIdentifier("FoldingCell", forIndexPath: indexPath) as! ReplyTableViewCell
         
-        let vc = BarsExample()
-        addChildViewController(vc)
-        vc.view.frame = cell.ThirdView.frame
-        vc.view.frame.origin.y -= 32
-        cell.ThirdView.addSubview(vc.view)
-        let radioButtonController = SSRadioButtonsController(buttons: cell.L1_Button, cell.L2_Button, cell.L3_Button,cell.L4_Button)
-
-        radioButtonController.delegate = self
-        radioButtonController.shouldLetDeSelect = true
-        
+      
+        imagesBinder(cell.imgContainer, images: [UIImage(named: "logo")!,UIImage(named: "logo")!])
         return cell
     }
     
@@ -95,11 +111,96 @@ class ReplyTableViewController: UITableViewController,SSRadioButtonControllerDel
         
         
     }
-
-    func didSelectButton(aButton: UIButton?) {
-        print(aButton)
-    }
-
     
+    //MARK:- Data Binder
+//    func dataBinder(cell:CommentTableViewCell,comment:Response)
+//    {
+//        
+//    }
+
+    func imagesBinder(containter:UIView,images:[UIImage])
+    {
+        let Xoffset = CGFloat(6)
+        let Yoffset = CGFloat(4)
+        for view in containter.subviews
+        {
+            if view.tag == 1
+            {
+                view.removeFromSuperview()
+            }
+        }
+        switch images.count
+        {
+        case 1:
+            let imgView = UIImageView(image: images.first)
+            imgView.frame = containter.frame
+            imgView.frame.origin = CGRectZero.origin
+            imgView.frame.size.width = tableView.frame.width - 40
+            containter.addSubview(imgView)
+            break;
+        case 2:
+            let img1 = UIImageView(image: images[1])
+            img1.frame = containter.frame
+            img1.frame.origin = CGRectZero.origin
+            img1.frame.size.width = tableView.frame.width - 30
+            img1.frame.size.width /= 2
+            containter.addSubview(img1)
+            let img2 = UIImageView(image: images[0])
+            img2.frame = img1.frame
+            img2.frame.origin.x += (img2.frame.size.width + Xoffset)
+            containter.addSubview(img2)
+            break;
+        case 3:
+            let img1 = UIImageView(image: images[0])
+            img1.frame = containter.frame
+            img1.frame.size.width = tableView.frame.width - 40
+            img1.frame.origin = CGRectZero.origin
+            img1.frame.size.width /= 2
+            img1.frame.size.height += Yoffset
+            containter.addSubview(img1)
+            let img2 = UIImageView(image: images[1])
+            img2.frame = img1.frame
+            img2.frame.origin.x += (img2.frame.size.width + Xoffset)
+            img2.frame.size.height /= 2
+            containter.addSubview(img2)
+            let img3 = UIImageView(image: images[2])
+            img3.frame = img2.frame
+            img3.frame.origin.y += img3.frame.size.height + Yoffset
+            containter.addSubview(img3)
+            break;
+        case 4:
+            let img1 = UIImageView(image: images[0])
+            img1.frame = containter.frame
+            img1.frame.size.width = tableView.frame.width - 40
+            img1.frame.origin = CGRectZero.origin
+            img1.frame.size.width /= 2
+            img1.frame.size.height /= 2
+            
+            containter.addSubview(img1)
+            let img2 = UIImageView(image: images[1])
+            img2.frame = img1.frame
+            img2.frame.origin.x += (img2.frame.size.width + Xoffset)
+            containter.addSubview(img2)
+            let img3 = UIImageView(image: images[2])
+            img3.frame = img2.frame
+            img3.frame.origin.y += img3.frame.size.height + Yoffset
+            containter.addSubview(img3)
+            let img4 = UIImageView(image: images[3])
+            img4.frame = img3.frame
+            img4.frame.origin.x = img1.frame.origin.x
+            containter.addSubview(img4)
+            break;
+        default:
+            containter.removeFromSuperview()
+            break;
+        }
+        for  view in containter.subviews
+        {
+            view.tag = 1
+            view.layer.masksToBounds = false
+            view.layer.cornerRadius = 5
+            view.clipsToBounds = true
+        }
+    }
 
 }
